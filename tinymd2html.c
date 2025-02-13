09f058531137b13c20a73b663b725fd4077bcff6
Stephen M. Cameron <stephenmcameron@gmail.com>
2025-02-13 12:46:21 -0500

    make it read input file
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- tinymd2html.c
+++ tinymd2html.c
@@ -19,10 +19,133 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct file_contents {
+	char **line;
+	int lines_allocated, lines_used;
+};
+
+static void usage(void)
+{
+	fprintf(stderr, "usage: tinymd2html -i input-file -o output-file\n");
+	exit(1);
+}
+
+static void parse_options(int argc, char *argv[], char **input_filename, char **output_filename)
+{
+	int c;
+
+	*input_filename = NULL;
+	*output_filename = NULL;
+
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"input", required_argument, 0,  'i' },
+			{"output",required_argument, 0,  'o' },
+			{0, 0, 0, 0 },
+		};
+
+		c = getopt_long(argc, argv, "i:o:", long_options, &option_index);
+		if (c == -1)
+			break;
+		switch (c) {
+		case 'i':
+			*input_filename = optarg;
+			break;
+		case 'o':
+			*output_filename = optarg;
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+}
+
+static int read_input_file(char *input_filename, struct file_contents *input)
+{
+	FILE *inputfile;
+
+	input->line = calloc(1024, sizeof(*input->line));
+	for (int i = 0; i < 1024; i++)
+		input->line[i] = NULL;
+	input->lines_allocated = 1024;
+	input->lines_used = 0;
+
+	if (input_filename == NULL || strcmp(input_filename, "-") == 0) {
+		inputfile = stdin;
+	} else {
+		inputfile = fopen(input_filename, "r");
+		if (!inputfile) {
+			fprintf(stderr, "Failed to open %s for input: %s\n", input_filename, strerror(errno));
+			exit(1);
+		}
+	}
+
+	do {
+		char buffer[1024];
+
+		char *l = fgets(buffer, sizeof(buffer), inputfile); /* Read a line from input file into buffer */
+		if (!l) {
+			if (feof(inputfile))
+				break;
+			fprintf(stderr, "Error reading file %s at line %d\n", input_filename, input->lines_used + 1);
+				break;
+		}
+
+		/* Allocate more lines if need be */
+		if (input->lines_used == input->lines_allocated) {
+			char **tmp = realloc(input->line, 2 * input->lines_allocated * sizeof(*input->line));
+			if (!tmp) {
+				fprintf(stderr, "Out of memory reading input file at line %d\n", input->lines_used + 1);
+				exit(1);
+			}
+			input->line = tmp;
+			input->lines_allocated *= 2;
+		}
+		input->line[input->lines_used] = strdup(buffer);
+		input->lines_used++;
+	} while (1);
+	fclose(inputfile);
+	return 0;
+}
+
+static void dump_file_contents(FILE *f, struct file_contents *input)
+{
+	for (int i = 0; i < input->lines_used; i++)
+		fprintf(f, "%s", input->line[i]);
+}
 
 int main(int argc, char *argv[])
 {
+	char *input_filename, *output_filename;
+	FILE *outputfile;
+	struct file_contents input;
+
+	parse_options(argc, argv, &input_filename, &output_filename);
+	if (output_filename == NULL || strcmp(output_filename, "-") == 0) {
+		outputfile = stdout;
+	} else {
+		outputfile = fopen(output_filename, "w+");
+		if (!outputfile) {
+			fprintf(stderr, "Failed to open %s for output: %s\n", output_filename, strerror(errno));
+			exit(1);
+		}
+	}
+
+	read_input_file(input_filename, &input);
+	dump_file_contents(outputfile, &input);
+
+	fclose(outputfile);
+
 	return 0;
 }
 
